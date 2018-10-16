a7aabc69f255ccca73d0c77187179004044ec472
kasulejoseph <kasule06joseph@gmail.com>
2018-10-16 18:00:37 +0300

    add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flasgger import Swagger
+from flask_cors import CORS
 from app.views.orders import main
 from app.auth.views import auth
 from app.auth import views
 from instance.config import app_config
 
 app = Flask(__name__, instance_relative_config=True)
+CORS(app)
 app.config.from_object(app_config["development"])
 Swagger(app)
 app.register_blueprint(main)
